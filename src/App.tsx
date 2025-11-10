6cdaad1022d3702790efb0be092bb67cbfcbcd95
Pedro Antônio <pedroantonioytbr@gmail.com>
2025-11-10 14:18:01 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ function App() {
   return (
     <main className="App">
       <h1>Pedro Antônio</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, dolore nobis? Quas, eaque. Itaque earum dolor vel excepturi possimus numquam eius accusantium! Provident deserunt, fugiat ipsum perferendis cupiditate omnis quisquam.</p>
     </main>
   )
 }

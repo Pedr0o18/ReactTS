76143d8c3478d58308be19a7a0a4796ce48e77ec
Pedro Antônio <pedroantonioytbr@gmail.com>
2025-11-25 11:22:56 -0300

    fix: prevent immediate function call in removeTask and replace array mutation with immutable state update

--- src/components/Tasks.tsx
+++ src/components/Tasks.tsx
@@ -1,3 +1,6 @@
+import React, { useState } from "react"
+
+
 type Task = {
     id: number
     title: string
@@ -6,19 +9,41 @@ type Task = {
 
 const Tasks = () => {
 
-    const tasks: Task[] = [
-        {id: 1, title: "Estudar", done: false}
-    ]
+    const [tasks, setTasks] = useState<Task[]>([
+        { id: 1, title: "Estudar", done: false }
+    ])
+
+    const [task, setTask] = useState<string>("")
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setTask(event.target.value)
+    }
+
+    const addTask = (event: React.MouseEvent<HTMLButtonElement>) => {
+        event.preventDefault()
+        const newTask: Task = {id: tasks.length + 1, title: task, done: false }
+        setTasks([...tasks, newTask])
+        console.log(tasks)
+        setTask("")
+    }
+
+    const removeTask = (id: number) => {
+        setTasks((prev) => prev.filter((task) => task.id !== id))
+    }
 
   return (
     <div className="flex flex-col justify-center items-center text-3xl bg-gray-700 text-white p-4 gap-2" >
         <h1>Tarefas Tipadas</h1>
-        <ul>
-            {
-                tasks.map((task) => (
-                    <li key={task.id} ><strong>Titulo:</strong> {task.title} <strong>Estado:</strong> {task.done ? "Feita" : "Não Finalizada" }</li>
-                ))
-            }
+        <div>
+            <input type="text" placeholder="tarefa" onChange={handleChange} value={task} className="border-2 p-2" />
+            <button onClick={addTask} className="button" >Salvar</button>
+        </div>
+            <ul>
+        {
+            tasks.map((task) => (
+                <li key={task.id} ><strong>Titulo:</strong> {task.id} {task.title} <strong>Estado:</strong> {task.done ? "Feita" : "Não Finalizada" } <button onClick={() => removeTask(task.id)} className="button" >Remover</button></li>
+            ))
+        }
         </ul>
     </div>
   )
